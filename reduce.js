1d25f0791b6fbdbe8cb08a55fd0196b7f525bf0b
aaron <aaronmlucia@gmail.com>
2021-10-28 17:45:29 -0400

    done one

--- reduce.js
+++ reduce.js
@@ -6,7 +6,13 @@ Examples:
     extractValue(arr,'name') // ['Elie', 'Tim', 'Matt', 'Colt']
 */
 
-function extractValue(arr, key) {}
+function extractValue(arr, key) {
+    let resultArr = [];
+    return arr.reduce(function(acc, nextVal){
+        acc.push(nextVal[key]);
+        return acc;
+    }, resultArr)
+}
 
 /*
 Write a function called vowelCount which accepts a string and returns an object with the keys as the vowel and the values as the number of times the vowel appears in the string. This function should be case insensitive so a lowercase letter and uppercase letter should count
@@ -19,7 +25,12 @@ Examples:
     vowelCount('I Am awesome and so are you') // {i: 1, a: 4, e: 3, o: 3, u: 1};
 */
 
-function vowelCount(str) {}
+function vowelCount(str) {
+    let tempArr = str.toLowerCase().split('');
+    return tempArr.reduce(acc, next){
+        
+    }
+}
 
 /*
 Write a function called addKeyAndValue which accepts an array of objects and returns the array of objects passed to it with each object now including the key and value passed to the function.
